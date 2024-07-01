1b1cae8ddcbb50014e5ac9785916e552f09999b2
Johab Freitas <freitasjohab@gmail.com>
2024-07-01 17:00:54 -0300

    Projeto inicial

---
+++ src/main/java/br/com/johabfreitas/desenvolvimento_java_com_ia/DesenvolvimentoJavaComIaApplication.java
@@ -0,0 +1,13 @@
+package br.com.johabfreitas.desenvolvimento_java_com_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesenvolvimentoJavaComIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesenvolvimentoJavaComIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/johabfreitas/desenvolvimento_java_com_ia/DesenvolvimentoJavaComIaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.johabfreitas.desenvolvimento_java_com_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesenvolvimentoJavaComIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
