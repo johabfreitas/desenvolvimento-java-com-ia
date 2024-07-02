4073f11964ff2c851fb55f17e344fbf0cd94e117
Johab Freitas <freitasjohab@gmail.com>
2024-07-02 13:18:20 -0300

    Extendendo classes Feature e News de BaseItem

--- src/main/java/br/com/johabfreitas/desenvolvimento_java_com_ia/model/News.java
+++ src/main/java/br/com/johabfreitas/desenvolvimento_java_com_ia/model/News.java
@@ -1,24 +1,9 @@
 package br.com.johabfreitas.desenvolvimento_java_com_ia.model;
 
-public class News {
+import jakarta.persistence.Entity;
 
-    private String icon;
-    private String description;
+@Entity(name = "tb_news")
+public class News extends BaseItem {
 
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
+    
 }
