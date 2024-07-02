bb71f28418cda0992473c146c6c9fa0bd280e418
Johab Freitas <freitasjohab@gmail.com>
2024-07-02 16:44:33 -0300

    Implementando controller e swagger

--- src/main/java/br/com/johabfreitas/desenvolvimento_java_com_ia/repository/UserRepository.java
+++ src/main/java/br/com/johabfreitas/desenvolvimento_java_com_ia/repository/UserRepository.java
@@ -5,5 +5,5 @@ import br.com.johabfreitas.desenvolvimento_java_com_ia.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    boolean existsByAccoutNumber(String accountNumber);
+    //boolean existsByAccoutNumber(String accountNumber);
 }
