e3960f3aa9ab8e355c9bc1f884a04c82327b5e95
Johab Freitas <freitasjohab@gmail.com>
2024-07-02 11:35:02 -0300

    Criado as classes

---
+++ src/main/java/br/com/johabfreitas/desenvolvimento_java_com_ia/model/Card.java
@@ -0,0 +1,24 @@
+package br.com.johabfreitas.desenvolvimento_java_com_ia.model;
+
+public class Card {
+
+    private String number;
+    private Number limit;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Number getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Number limit) {
+        this.limit = limit;
+    }
+
+}
---
+++ src/main/java/br/com/johabfreitas/desenvolvimento_java_com_ia/model/Feature.java
@@ -0,0 +1,24 @@
+package br.com.johabfreitas.desenvolvimento_java_com_ia.model;
+
+public class Feature {
+
+    private String icon;
+    private String description;
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
