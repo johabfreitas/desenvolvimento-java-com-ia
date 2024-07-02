e79858c2c2dd48768fc5289b3c1f213d55f8f8ff
Johab Freitas <freitasjohab@gmail.com>
2024-07-02 13:45:47 -0300

    Criando Interface de repositório para User

---
+++ src/main/java/br/com/johabfreitas/desenvolvimento_java_com_ia/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.johabfreitas.desenvolvimento_java_com_ia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.johabfreitas.desenvolvimento_java_com_ia.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
